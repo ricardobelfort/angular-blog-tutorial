4f1af01356a84fc350fbb1a0c62a831474eeaeba
Ricardo Belfort <rbelfort2004@gmail.com>
2024-07-03 08:54:14 -0400

    feat: criando os primeiros componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PostsComponent } from './pages/posts/posts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+    data: {
+      title: 'Home',
+    },
+  },
+  {
+    path: 'posts',
+    component: PostsComponent,
+    data: {
+      title: 'Artigos',
+    },
+  },
+];
